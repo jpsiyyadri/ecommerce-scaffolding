3b88ae02f707c456312a87ef985a1a30bfac34d9
Jai Prakash <jpsiyyadri@gmail.com>
2021-02-20 13:25:06 +0530

    upd: app js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
+    console.log("request has been received")
     console.log(req.url, req.method, req.headers)
     res.setHeader('Content-type', 'text/html')
+    console.log("response has been served")
     return res.write("Hello world")
 })
 
+console.log("started node server on port 5000")
 server.listen(5000)
