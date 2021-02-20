66b7aef24d897abe18d97f99474b359de76044c8
Jai Prakash <jpsiyyadri@gmail.com>
2021-02-20 12:35:31 +0530

    ADDED: app js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers)
+    console.log(req.url, req.method, req.headers)
     res.setHeader('Content-type', 'text/html')
     return res.write("Hello world")
 })
